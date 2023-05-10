ba9b96f174b2bdc4519a7bd9871b614b97b17de1
nyxb <contact@nyxb.xyz>
2023-05-10 11:41:49 +0200

    ✨ feat(_utils.ts): add utility functions for downloading files, parsing git URIs, and sending fetch requests This commit adds a new file `_utils.ts` which contains utility functions for downloading files, parsing git URIs, sending fetch requests, and other miscellaneous functions. The `download` function downloads a file from a given URL and saves it to a specified file path. The `parseGitURI` function parses a git URI and returns an object containing the repository name, subdirectory, and reference. The `sendFetch` function sends a fetch request with optional headers and an optional agent. Other functions include `debug`, `cacheDirectory`, `normalizeHeaders`, `currentShell`, and `startShell`.
    ✨ feat(providers.ts): add template providers for GitHub, GitLab, Bitbucket, and SourceHut
    This commit adds a new file `providers.ts` which contains template providers for GitHub, GitLab, Bitbucket, and SourceHut

---
+++ src/_utils.ts
@@ -0,0 +1,130 @@
+import { createWriteStream, existsSync } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { spawnSync } from 'node:child_process'
+import { readFile, writeFile } from 'node:fs/promises'
+import { homedir } from 'node:os'
+import { promisify } from 'node:util'
+import type { Agent } from 'node:http'
+import consolji from 'consolji'
+import { relative, resolve } from 'nyxpath'
+import { fetch } from 'fetch-for-all'
+import createHttpsProxyAgent from 'https-proxy-agent'
+import type { GitInfo } from './types'
+
+export async function download(
+   url: string,
+   filePath: string,
+   options: { headers?: Record<string, string> } = {},
+) {
+   const infoPath = `${filePath}.json`
+   const info: { etag?: string } = JSON.parse(
+      await readFile(infoPath, 'utf8').catch(() => '{}'),
+   )
+
+   const headResponse = await sendFetch(url, {
+      method: 'HEAD',
+      headers: options.headers,
+   }).catch(() => undefined)
+   const etag = headResponse?.headers.get('etag')
+   if (info.etag === etag && existsSync(filePath)) {
+      // Already downloaded
+      return
+   }
+   info.etag = etag
+
+   const response = await sendFetch(url, { headers: options.headers })
+   if (response.status >= 400) {
+      throw new Error(
+      `Failed to download ${url}: ${response.status} ${response.statusText}`,
+      )
+   }
+
+   const stream = createWriteStream(filePath)
+   await promisify(pipeline)(response.body as any, stream)
+
+   await writeFile(infoPath, JSON.stringify(info), 'utf8')
+}
+
+const inputRegex
+  = /^(?<repo>[\w.-]+\/[\w.-]+)(?<subdir>[^#]+)?(?<ref>#[\w.-]+)?/
+
+export function parseGitURI(input: string): GitInfo {
+   const m = input.match(inputRegex)?.groups
+   return <GitInfo>{
+      repo: m.repo,
+      subdir: m.subdir || '/',
+      ref: m.ref ? m.ref.slice(1) : 'main',
+   }
+}
+
+export function debug(...arguments_) {
+   if (process.env.DEBUG)
+      consolji.debug('[gitize]', ...arguments_)
+}
+
+interface InternalFetchOptions extends Exclude<RequestInit, 'headers'> {
+   headers?: Record<string, string>
+   agent?: Agent
+}
+
+export async function sendFetch(
+   url: string,
+   options: InternalFetchOptions = {},
+) {
+   if (!options.agent) {
+      const proxyEnv
+      = process.env.HTTPS_PROXY
+      || process.env.https_proxy
+      || process.env.HTTP_PROXY
+      || process.env.http_proxy
+      if (proxyEnv)
+         options.agent = createHttpsProxyAgent(proxyEnv)
+   }
+
+   if (options?.headers)
+      options.headers = normalizeHeaders(options.headers)
+
+   return await fetch(url, options)
+}
+
+export function cacheDirectory() {
+   return process.env.XDG_CACHE_HOME
+      ? resolve(process.env.XDG_CACHE_HOME, 'gitize')
+      : resolve(homedir(), '.cache/gitize')
+}
+
+export function normalizeHeaders(headers: Record<string, string> = {}) {
+   const normalized: Record<string, string> = {}
+   for (const [key, value] of Object.entries(headers)) {
+      if (!value)
+         continue
+
+      normalized[key.toLowerCase()] = value
+   }
+   return normalized
+}
+
+// -- Experimental --
+
+export function currentShell() {
+   if (process.env.SHELL)
+      return process.env.SHELL
+
+   if (process.platform === 'win32')
+      return 'cmd.exe'
+
+   return '/bin/bash'
+}
+
+export function startShell(cwd: string) {
+   cwd = resolve(cwd)
+   const shell = currentShell()
+   consolji.info(
+    `(experimental) Opening shell in ${relative(process.cwd(), cwd)}...`,
+   )
+   spawnSync(shell, [], {
+      cwd,
+      shell: true,
+      stdio: 'inherit',
+   })
+}
---
+++ src/providers.ts
@@ -0,0 +1,68 @@
+import type { TemplateProvider } from './types'
+import { parseGitURI } from './_utils'
+
+export const github: TemplateProvider = (input, options) => {
+   const parsed = parseGitURI(input)
+   const github = process.env.GITIZE_GITHUB_URL || 'https://github.com'
+   return {
+      name: parsed.repo.replace('/', '-'),
+      version: parsed.ref,
+      subdir: parsed.subdir,
+      headers: {
+         authorization: options.auth ? `Bearer ${options.auth}` : undefined,
+      },
+      url: `${github}/${parsed.repo}/tree/${parsed.ref}${parsed.subdir}`,
+      tar: `${github}/${parsed.repo}/archive/${parsed.ref}.tar.gz`,
+   }
+}
+
+export const gitlab: TemplateProvider = (input, options) => {
+   const parsed = parseGitURI(input)
+   const gitlab = process.env.GITIZE_GITLAB_URL || 'https://gitlab.com'
+   return {
+      name: parsed.repo.replace('/', '-'),
+      version: parsed.ref,
+      subdir: parsed.subdir,
+      headers: {
+         authorization: options.auth ? `Bearer ${options.auth}` : undefined,
+      },
+      url: `${gitlab}/${parsed.repo}/tree/${parsed.ref}${parsed.subdir}`,
+      tar: `${gitlab}/${parsed.repo}/-/archive/${parsed.ref}.tar.gz`,
+   }
+}
+
+export const bitbucket: TemplateProvider = (input, options) => {
+   const parsed = parseGitURI(input)
+   return {
+      name: parsed.repo.replace('/', '-'),
+      version: parsed.ref,
+      subdir: parsed.subdir,
+      headers: {
+         authorization: options.auth ? `Bearer ${options.auth}` : undefined,
+      },
+      url: `https://bitbucket.com/${parsed.repo}/src/${parsed.ref}${parsed.subdir}`,
+      tar: `https://bitbucket.org/${parsed.repo}/get/${parsed.ref}.tar.gz`,
+   }
+}
+
+export const sourcehut: TemplateProvider = (input, options) => {
+   const parsed = parseGitURI(input)
+   return {
+      name: parsed.repo.replace('/', '-'),
+      version: parsed.ref,
+      subdir: parsed.subdir,
+      headers: {
+         authorization: options.auth ? `Bearer ${options.auth}` : undefined,
+      },
+      url: `https://git.sr.ht/~${parsed.repo}/tree/${parsed.ref}/item${parsed.subdir}`,
+      tar: `https://git.sr.ht/~${parsed.repo}/archive/${parsed.ref}.tar.gz`,
+   }
+}
+
+export const providers: Record<string, TemplateProvider> = {
+   github,
+   gh: github,
+   gitlab,
+   bitbucket,
+   sourcehut,
+}
