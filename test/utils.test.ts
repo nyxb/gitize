cfee578f1d7427072d9697a680deff4316a16fb0
nyxb <contact@nyxb.xyz>
2023-05-10 11:44:56 +0200

    🚀 feat(getgit.test.ts, utils.test.ts): add tests for downloadTemplate and parseGitURI functions The `downloadTemplate` function is tested to ensure that it can clone a repository from GitHub and that it throws an error when trying to clone to an existing directory. The `parseGitURI` function is tested to ensure that it can correctly parse a GitHub URI and extract the repository, subdirectory, and ref.

---
+++ test/utils.test.ts
@@ -0,0 +1,20 @@
+import { expect, it, describe } from "vitest";
+import { parseGitURI } from "../src/_utils";
+
+describe("parseGitURI", () => {
+  const defaults = { repo: "org/repo", subdir: "/", ref: "main" };
+  const tests = [
+    { input: "org/repo", output: {} },
+    { input: "org/repo#ref", output: { ref: "ref" } },
+    { input: "org/repo/foo/bar", output: { subdir: "/foo/bar" } },
+  ];
+
+  for (const test of tests) {
+    it(test.input, () => {
+      expect(parseGitURI(test.input)).toMatchObject({
+        ...defaults,
+        ...test.output,
+      });
+    });
+  }
+});
