205b98738e95b66b31e8ab7ed6b1c825221aac0f
nyxb <contact@nyxb.xyz>
2023-05-10 11:42:09 +0200

    🆕 feat(types.ts): add GitInfo, TemplateInfo and TemplateProvider interfaces This commit adds three new interfaces to the types.ts file. GitInfo interface defines the properties of a git repository, TemplateInfo interface defines the properties of a template, and TemplateProvider interface defines the properties of a function that returns a TemplateInfo object. These interfaces are used to provide type safety and improve code readability.

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface GitInfo {
+   provider: 'github' | 'gitlab' | 'bitbucket' | 'sourcehut'
+   repo: string
+   subdir: string
+   ref: string
+}
+
+export interface TemplateInfo {
+   name: string
+   tar: string
+   version?: string
+   subdir?: string
+   url?: string
+   defaultDir?: string
+   headers?: Record<string, string>
+
+   // Added by gitize
+   source?: never
+   dir?: never
+
+   [key: string]: any
+}
+
+export type TemplateProvider = (
+   input: string,
+   options: { auth?: string }
+) => TemplateInfo | Promise<TemplateInfo> | null
